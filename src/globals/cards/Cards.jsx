7b8a2bca8e323a90764095eec29627e254c3b13a
PwntasticKev <pwntastickevin@gmail.com>
2021-01-11 21:07:00 -0700

    cards added and alot of styles

---
+++ src/globals/cards/Cards.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import '../../scss/item-cards.scss'
+import { cardData } from './cardData'
+
+
+export default class componentName extends Component {
+	constructor() {
+		super()
+		this.state = {
+			cards: []
+		}
+
+	}
+
+	async componentDidMount() {
+		try {
+			// const { data: cards } = await axios.post('/cards')
+			this.setState({
+				cards: cardData
+			})
+		}
+		catch(error) {
+			console.error(error)
+		}
+	}
+
+  render() {
+
+	let cards = this.state.cards.map( v => 
+		<div className="card-ctn__card">
+			<div className="card-ctn__card__img-ctn">
+				<img src={v.url} alt="💩" className="card-ctn__card__img-ctn--img"/>
+				{ 
+					v.workouts !== ''
+					? 
+					<div className="card-ctn__card__img-ctn--workout-count">
+						<>{v.workouts}</>
+						<>Workouts</>
+						
+					</div> 
+					: 
+					<></>
+				}
+			</div>
+			<div className="card-ctn__card__desc-container">
+				<div className="d-flex align-items-center">
+					<div className="card-ctn__card__desc-container--title">
+						Lake Innsicarra, Ireland-Pyramid
+					</div>
+					<div className="card-ctn__card__desc-container--profile">
+						<img src={v.profile} alt=""/>
+					</div>
+				</div>
+				<div className="card-ctn__card__desc-container__lower-title">
+					<div className="card-ctn__card__desc-container__lower-title--info">
+						<i className="far fa-clock"></i>
+						${v.time}
+					</div>
+					<div className="card-ctn__card__desc-container__lower-title--info">
+						<i className="fas fa-walking"></i>
+						${v.distance}
+					</div>
+				</div>
+				{ 
+					v.link !== ''
+					? 
+					<div className="card-ctn__card__desc-container__details">
+						VIEW DETAILS
+					</div> 
+					: 
+					<></>
+				}
+			</div>
+		</div>
+	);
+
+	return (
+		//I LIKE VUE. 🤫
+	  <div className="card-ctn">{cards}</div>
+	)
+  }
+}
---
+++ src/globals/cards/cardData.js
@@ -0,0 +1,47 @@
+export const cardData = [
+	{
+		title: '',
+		url: 'https://images.unsplash.com/photo-1436162716854-dcb9157bfac1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		profile: 'https://images.unsplash.com/photo-1436162716854-dcb9157bfac1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		distance: 9540,
+		link: '',
+		workouts: 0,
+		time: '5ms'
+	},
+	{
+		title: '',
+		url: 'https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		profile: 'https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		distance: 9540,
+		link: '',
+		workouts: 12,
+		time: '5ms'
+	},
+	{
+		title: '',
+		url: 'https://images.unsplash.com/photo-1501761095094-94d36f57edbb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		profile: 'https://images.unsplash.com/photo-1501761095094-94d36f57edbb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		distance: 9540,
+		link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+		workouts: 3,
+		time: '5ms'
+	},
+	{
+		title: '',
+		url: 'https://images.unsplash.com/photo-1517960413843-0aee8e2b3285?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		profile: 'https://images.unsplash.com/photo-1517960413843-0aee8e2b3285?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGFkdmVudHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		distance: 9540,
+		link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+		workouts: 0,
+		time: '5ms'
+	},
+	{
+		title: '',
+		url: 'https://images.unsplash.com/photo-1547469447-4afec158715b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		profile: 'https://images.unsplash.com/photo-1547469447-4afec158715b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8YWR2ZW50dXJlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+		distance: 9540,
+		link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+		workouts: 0,
+		time: '5ms'
+	},
+]
